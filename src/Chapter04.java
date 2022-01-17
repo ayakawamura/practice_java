6dc06c7b6b95ff46fe548cd6da1f9e670e681bf5
ayakawamura <ayakawamura.b@gmail.com>
2022-01-17 22:55:59 +0900

    変数、定数の指定

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main (String[] args) {
+		String name;
+		
+		name = "川村綾香";
+		System.out.println(name);
+		
+		name = "kawamura";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
